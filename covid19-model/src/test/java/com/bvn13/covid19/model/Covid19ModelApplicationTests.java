a635c29eda4a72fe7daa091addc9f15a2baca5d2
bvn13 <mail4bvn@gmail.com>
2020-04-03 16:51:54 +0300

    fixed build

--- covid19-model/src/test/java/com/bvn13/covid19/model/Covid19ModelApplicationTests.java
+++ covid19-model/src/test/java/com/bvn13/covid19/model/Covid19ModelApplicationTests.java
@@ -2,8 +2,16 @@ package com.bvn13.covid19.model;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Profile;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.support.AnnotationConfigContextLoader;
 
 @SpringBootTest
+@ContextConfiguration(
+        classes = { Covid19ModelConfig.class },
+        loader = AnnotationConfigContextLoader.class)
+@Profile("test")
 class Covid19ModelApplicationTests {
 
     @Test
--- covid19-scheduler/src/test/java/com/bvn13/covid19/scheduler/Covid19SchedulerApplicationTests.java
+++ covid19-scheduler/src/test/java/com/bvn13/covid19/scheduler/Covid19SchedulerApplicationTests.java
@@ -1,9 +1,19 @@
 package com.bvn13.covid19.scheduler;
 
+import com.bvn13.covid19.model.Covid19ModelConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Profile;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.support.AnnotationConfigContextLoader;
 
 @SpringBootTest
+@EnableJpaRepositories("com.bvn13.covid19.scheduler")
+@ContextConfiguration(
+        classes = { Covid19ModelConfig.class },
+        loader = AnnotationConfigContextLoader.class)
+@Profile("test")
 class Covid19SchedulerApplicationTests {
 
     @Test
